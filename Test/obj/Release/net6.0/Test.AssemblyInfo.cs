9d69ff3bd061f42d29aca0486a859baf1275fe4f
Evsty <75947237+OfficialEvsty@users.noreply.github.com>
2024-06-05 21:51:40 +0300

    edit
    fix

--- Test/obj/Release/net6.0/Test.AssemblyInfo.cs
+++ Test/obj/Release/net6.0/Test.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("FiguresTest")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("FiguresTest")]
-[assembly: System.Reflection.AssemblyTitleAttribute("FiguresTest")]
+[assembly: System.Reflection.AssemblyProductAttribute("Test")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
